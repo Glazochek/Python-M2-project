48dde1eb16d114498b3d2f1027d3786698d02da0
Glazochek <an9680411@gmail.com>
2025-11-18 19:19:04 +0100

    adding edges to vertices

--- functions.py
+++ functions.py
@@ -14,27 +14,40 @@ def get_console_size():
 
 
 def clear_screen():
-    try:
-        os.system('cls' if os.name == 'nt' else 'clear')
-    except:
-        print('\n' * 24)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def create_canvas(width, height):
     return [[' ' for _ in range(width)] for _ in range(height)]
 
 
-def render_canvas(canvas):
+def add_border(canvas):
     for row in canvas:
-        print(''.join(row))
+        canvas.insert(0, "#")
+        canvas.append("#")
+    print(canvas)
+    return canvas
+
+
+def render_canvas(canvas):
+    # for i in range(len(canvas)):
+    #     for j in range(len(canvas)):
+    #         if canvas[i][j] == canvas[i][j+1] == "*":
+    #             canvas[i].insert(j+1, " ")
+    # canvas = add_border(canvas)
+    print(*[''.join(row) for row in canvas], sep="\n")
 
 
 def rotate_x(point, angle):
-    return point
+    x, y, z = point
+    cos_a, sin_a = math.cos(angle), math.sin(angle)
+    return [(y * cos_a) - (z * sin_a), (y * sin_a) + (z * cos_a), x]
 
 
 def rotate_y(point, angle):
-    return point
+    x, y, z = point
+    cos_a, sin_a = math.cos(angle), math.sin(angle)
+    return [(x * cos_a) - (z * sin_a), (x * sin_a) + (z * cos_a), y]
 
 
 def rotate_z(point, angle):
@@ -45,5 +58,23 @@ def rotate_z(point, angle):
 
 def project(point, scale=3, offset=(10, 6)):
     x, y, z = point
-    # very basic projection
-    return int(offset[0] + x * scale), int(offset[1] + y * scale)
+    perspective = 1 / (1 + z / 2)
+    screen_x = int(offset[0] + x * scale * perspective)
+    screen_y = int(offset[1] + y * scale * perspective)
+    return screen_x, screen_y, z
+
+
+def draw_line(canvas, x0, y0, x1, y1, char='*'):
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    sx = 1 if x0 < x1 else -1
+    sy = 1 if y0 < y1 else -1
+    err = dx - dy
+    while True:
+        if 0 <= x0 < len(canvas[0]) and 0 <= y0 < len(canvas):
+            canvas[y0][x0] = char
+        if x0 == x1 and y0 == y1:
+            break
+        e2 = 2 * err
+        if e2 > -dy: err -= dy; x0 += sx
+        if e2 < dx: err += dx; y0 += sy
--- main.py
+++ main.py
@@ -5,7 +5,8 @@ This file for main actions
 """
 import time
 
-from functions import clear_screen, create_canvas, rotate_z, project, render_canvas
+from functions import clear_screen, create_canvas, rotate_z, project, render_canvas, rotate_y, rotate_x, \
+    get_console_size, draw_line
 from validation import check_email
 
 vertices = [
@@ -19,30 +20,42 @@ edges = [
     (0, 4), (1, 5), (2, 6), (3, 7)
 ]
 
+# cube_size = 12
+window_size = (30, 20)
+offset = (window_size[0] / 2, window_size[1] / 2)
+speed_rotation = (0.1, 0.1, 0)
 
-def animate_cube(duration=10, fps=15):
+
+def animate_cube(duration=50, fps=15):
     frame = 0
     start = time.time()
     while (time.time() - start) < duration:
         clear_screen()
-        canvas = create_canvas(20, 12)
-        angle_z = frame * 0.1
-        rotated = [rotate_z(v, angle_z) for v in vertices]
+        # x, y = get_console_size()
+        canvas = create_canvas(*window_size)
+        speed_x, speed_y, speed_z = speed_rotation
+        angle_x, angle_y, angle_z = frame * speed_x, frame * speed_y, frame * speed_z
+        rotated = []
 
-        for v in rotated:
-            x, y = project(v)
-            if 0 <= x < 20 and 0 <= y < 12:
-                canvas[y][x] = '*'
+        for vert in vertices:
+            v = rotate_x(vert, angle_x)
+            v = rotate_y(v, angle_y)
+            rotated.append(rotate_z(v, angle_z))
+
+        projected_v = [project(v, offset=offset) for v in rotated]
+        for s, e in edges:
+            draw_line(canvas, *projected_v[s][:-1], *projected_v[e][:-1])
+
         render_canvas(canvas)
         frame += 1
         time.sleep(1 / fps)
 
 
 def main():
-    email = input("Please enter your email: ")
-    if not check_email(email):
-        print("Invalid email, you will not see my beautiful 3D animation in console.")
-        return
+    # email = input("Please enter your email: ")
+    # if not check_email(email):
+    #     print("Invalid email, you will not see my beautiful 3D animation in console.")
+    #     return
     animate_cube()
 
 
